89c135f4045f6b405443e3af403990d55c18ca43
yong park <yong.park123456@gmail.com>
2023-02-09 12:45:33 -0600

    conversion a postfix

--- main.py
+++ main.py
@@ -6,4 +6,7 @@ r = '(a|b)*abb'
 # r = 'ab(a|b*)ab*ba'
 
 post = Postfix(r)
-post.infix_Transformation()
+new_infix = post.infix_Transformation()
+postfix = post.transform_postfix(new_infix)
+print(postfix)
+
--- postfix.py
+++ postfix.py
@@ -1,26 +1,111 @@
 class Postfix:
     
+    
+    
     def __init__(self,infix):
         self.infix = infix
         self.postfix = None
+        self.stack =[]
+        self.output = []
 
     def infix_Transformation(self):
         self.infix_array = []
         for value in range(len(self.infix)):
             self.infix_array.append(self.infix[value])
-            if self.infix[value] not in '|':
-                self.infix_array.append('.')
-
+            self.infix_array.append('.')
+                
         for value in range(len(self.infix_array)):
-            if self.infix_array[value] in '.':
-                if value+1 < len(self.infix_array):
-                    if self.infix_array[value+1] in '|)':
-                        self.infix_array[value] = ''
-        self.infix_array.pop(len(self.infix_array)-1)
+            if self.infix_array[value] in '|':
+                if self.infix_array[value-1] in ".":
+                    self.infix_array[value-1] = " "
+                if self.infix_array[value+1] in ".":
+                    self.infix_array[value+1] =" "
                     
-        print(self.infix_array)
+            elif self.infix_array[value] in '(':
+                if self.infix_array[value+1] in ".":
+                    self.infix_array[value+1] =" "
+                    
+            elif self.infix_array[value] in ')':
+                if self.infix_array[value-1] in ".":
+                    self.infix_array[value-1] =" "
+                    
+            elif self.infix_array[value] in '*':
+                if self.infix_array[value-1] in ".":
+                    self.infix_array[value-1] =" "
+                    
+            
+        self.infix_array.pop(len(self.infix_array)-1)
+        
+        self.data = ''.join([c for c in self.infix_array if c != ' '])
+        return self.data
 
-        
-        
-        
 
+    def transform_postfix(self,c):
+        print(c)
+        for l in c:
+            # print("====================")
+            # print(l)
+            # print("s: " + str(stack))
+            # print("o: " + str(output))
+            if l in "|()*.":
+                self.stack.append(l)
+
+                if l == ")":
+                    a = len(self.stack)-2
+                    while(self.stack[a] != "("):
+                        self.output.append(self.stack[a])
+                        self.stack[a]=""
+                        a = a-1
+                    self.stack[a] = ""
+                    self.stack[self.stack.index(")")] = ""
+                    while "" in self.stack:
+                        self.stack.remove("")
+                elif l == ".":
+                    try:
+                        if len(self.stack) > 1:
+                            # print("====================")
+                            # print("verdadero")
+                            # print("s: " + str(stack))
+                            # print("====================")
+                            if self.stack[len(self.stack)-2] == "*":
+                                self.output.append(self.stack[len(self.stack)-2])
+                                self.stack.pop(len(self.stack)-2)
+                            elif self.stack[len(self.stack)-2] == ".":
+                                self.output.append(self.stack[len(self.stack)-1])
+                                self.stack.pop(len(self.stack)-1)
+                    except:
+                        pass
+                elif l == "|":
+                    try:
+                        if len(self.stack) > 1:
+                            if self.stack[len(self.stack)-2] == "*":
+                                self.output.append(self.stack[len(self.stack)-2])
+                                self.stack.pop(len(self.stack)-2)
+                            elif self.stack[len(self.stack)-2] == ".":
+                                self.output.append(self.stack[len(self.stack)-2])
+                                self.stack.pop(len(self.stack)-2)
+                            elif self.stack[len(self.stack)-2] == "|":
+                                self.output.append(self.stack[len(self.stack)-1])
+                                self.stack.pop(self.stack[len(self.stack)-1])
+                    except:
+                        pass
+                elif l =="*":
+                    try:
+                        if len(self.stack) > 1:
+                            if self.stack[len(self.stack)-2] == "*":
+                                self.output.append(self.stack[len(self.stack)-1])
+                                self.stack.pop(self.stack[len(self.stack)-1])
+                    except:
+                        pass
+                            
+            else:
+                self.output.append(l)
+        while(len(self.stack) > 0):
+            self.output.append(self.stack[len(self.stack)-1])
+            self.stack.pop(len(self.stack)-1)
+
+        # print("output: " + str(output))
+        # print("stack: " + str(stack))
+        
+        #convertir el postfix a afd
+        return self.output
